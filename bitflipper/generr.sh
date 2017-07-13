6ee9b7d7a5221282b5e1cb5a288a0b629fb8f23a
fbmoreira <fbmoreira@inf.ufrgs.br>
2017-07-13 13:27:39 -0300

    improved input for generr.sh

--- bitflipper/generr.sh
+++ bitflipper/generr.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 
-#receives 1 argument, executable binary which will have bits flipped
+set -o errexit -o nounset -o pipefail -o errtrace -o posix
 
+#first argument is executable binary which will have bits flipped, second argument is number of threads, following arguments are passed to the program
+
+export OMP_NUM_THREADS=$2
+origbin=$1
+
+shift 2
 
 #calculate offsets based on argument binary $1 == basebin
-readelf -S $1 > ELFFILE
+readelf -S $origbin > ELFFILE
 init_off=$(grep ELFFILE -e ".text" | awk '{print $6}')  		#obtains offset of .text in the binary
 init_offset=$((16#$init_off)) 						#converts to decimal base
 text_size=$(grep ELFFILE -e ".text" -A 1 | awk '{print $1}' | tail -1 ) #obtains size of text 
@@ -13,7 +19,7 @@ end_text_size=$((16#$text_size))						#converts to decimal base
 end_offset=$(echo "obase=10;ibase=10;$init_offset+$end_text_size" | bc)	#sums to get final bytes
 echo $init_offset
 echo $end_offset
-./$1 ../../cbench/automotive_qsort_data/10.dat > resultBASE		#obtains control output for comparison
+./$origbin $@ > resultBASE	#obtains control output for comparison
 rm ELFFILE	#clean up
 
 # loop to generate silent errors
@@ -21,10 +27,10 @@ for i in $(eval echo "{$init_offset..$end_offset}");
 do
         for k in `seq 0 7`;
         do
-                ./dorep $1 hackbin $i $k						#flip bit #k of byte #i in binary $1, output in binary "hackbin"
+                ./dorep $origbin hackbin $i $k						#flip bit #k of byte #i in binary $1, output in binary "hackbin"
                 chmod +x hackbin				
-                ./hackbin ../../cbench/automotive_qsort_data/10.dat > resultFLIP	#generates output of execution with flipped bit. CAREFUL, syscalls with flipped bits can have very weird effects on the system
-                ps=${PIPESTATUS[0]}					
+                timeout --preserve-status 10s ./hackbin $@ | tee resultFLIP	#generates output of execution with flipped bit. CAREFUL, syscalls with flipped bits can have very weird effects on the system
+		ps=${PIPESTATUS[0]}							#pipe status of binary execution					
                 echo "return code: $ps" | tee -a hackout		
                 if [ $ps -eq 0 ]							#if return code is normal, i.e. OS did not detect anything unusual
                 then
@@ -41,7 +47,7 @@ do
 done
 
 rm resultBASE								#clean up
-awk '{print $1}' interesting_errors > $1_serror_BYTES 			#generates file with silent error BYTE in each line
-awk '{print $2}' interesting_errors > $1_serror_BITS			#generates file with silent error BIT in each line
+awk '{print $1}' interesting_errors > $origbin_serror_BYTES 			#generates file with silent error BYTE in each line
+awk '{print $2}' interesting_errors > $origbin_serror_BITS			#generates file with silent error BIT in each line
 
 
